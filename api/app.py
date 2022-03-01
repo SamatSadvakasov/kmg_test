b869864339b7da981ed2f026168d80d0b6db3a61
samat <sadvakasov.samat@gmail.com>
2022-03-01 20:23:17 +0600

    Fourth task was done

--- api/app.py
+++ api/app.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from typing import Optional
+from datetime import datetime
+from database.db import *
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    """SWAGGER"""
+    return RedirectResponse(url='/docs')
+
+
+@app.get("/fields/{field_id}/data")
+def get_field_data_handle(field_id: Optional[int] = 1,
+                          negative: Optional[bool] = False,
+                          start_time: Optional[str] = datetime(2020, 6, 3, 17, 58, 16).strftime("%Y-%m-%d %H:%M:%S"),
+                          finish_time: Optional[str] = datetime(2020, 6, 3, 17, 58, 36).strftime("%Y-%m-%d %H:%M:%S")):
+    """Получить данные месторождения по идентификатору"""
+    start = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+    finish = datetime.strptime(finish_time, "%Y-%m-%d %H:%M:%S")
+    if negative:
+        return get_negative_field_data(field_id, start, finish)
+    return get_positive_field_data(field_id, start, finish)
--- database/db.py
+++ database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError
-from .models import Base, Field
+from .models import Base, Field, FieldData
 
 engine = create_engine("postgresql+psycopg2://kmg:qwerty123@127.0.0.1:10001/kmg")
 Session = sessionmaker(bind=engine)
@@ -39,5 +39,17 @@ def save_field_data(instances: list):
     return {"status": "Success", "message": "Success"}
 
 
-def get_fields() -> list:
-    return session.query(Field).all()
+def get_positive_field_data(field_id: int, start, finish) -> list:
+    query = session.query(FieldData).filter(FieldData.field_id == field_id)\
+        .filter(FieldData.start_datetime >= start)\
+        .filter(FieldData.start_datetime <= finish)\
+        .filter(FieldData.value != 0)
+    return query.all()
+
+
+def get_negative_field_data(field_id: int, start, finish) -> list:
+    query = session.query(FieldData).filter(FieldData.field_id == field_id) \
+        .filter(FieldData.start_datetime >= start) \
+        .filter(FieldData.start_datetime <= finish) \
+        .filter(FieldData.value == 0)
+    return query.all()
